9fb0ae38641d0b4a0f9ebd4655d373f6fdabcc0c
Juan Linares <juanzlinares29@gmail.com>
2025-01-27 23:14:23 -0500

    Prioridad gravedad

---
+++ src/Strategy/StrategySeverityPriority.java
@@ -0,0 +1,23 @@
+package src.Strategy;
+
+import src.Model.Emergency;
+import src.Model.Interfaces.IPriority;
+
+public class StrategySeverityPriority implements IPriority {
+
+    @Override
+    public int calculatePriority(Emergency emergency) {
+
+        switch (emergency.getSeverity()) {
+            case LOW:
+                return 1;
+            case MEDIUM:
+                return 2;
+            case HIGH:
+                return 3;
+            default:
+                return 1;
+        }
+    }
+
+}
